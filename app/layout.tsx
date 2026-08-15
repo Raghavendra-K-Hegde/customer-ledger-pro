67668b81ac2c6ac03500e84ceddd5c840dd5af78
Raghavendra-K-Hegde <raghu.hegde1986@gmail.com>
2026-08-16 00:26:44 +0530

    Add RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "RaghDemo",
+  description: "Customer Ledger Pro",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
